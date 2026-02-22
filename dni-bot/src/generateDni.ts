11667025186c83a48f4d432e98e3d2acb4839ae5
Argentina-Roleplay-2025 <2pj.vladimir250426@gmail.com>
2026-02-22 01:49:24 -0300

    Update generateDni.ts

--- dni-bot/src/generateDni.ts
+++ dni-bot/src/generateDni.ts
@@ -14,7 +14,7 @@ const POS = {
   apellido:         { x: 280, y: 150 },
   nombre:           { x: 280, y: 215 },
   nacionalidad:     { x: 565, y: 200 },
-  sexo:             { x: 280, y: 280 },
+  sexo:             { x: 280, y: 265 },
   fecha_nacimiento: { x: 280, y: 355 },
   fecha_emision:    { x: 565, y: 295 },
   documento:        { x: 119, y: 415 },
